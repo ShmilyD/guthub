03ebd21d96ac76501cd28f34caa0b36c4ae94b7b
zhangzhidong <984281302@qq.com>
2016-12-19 17:16:53 +0800

    php

--- dan.php
+++ dan.php
@@ -1,3 +1,9 @@
+<?php
+require_once "jssdk.php";
+// appId  和 秘钥
+$jssdk = new JSSDK("wx3535700c9793a7f9", "0e7e2ef647d4070cce1656bab6cd3846");
+$signPackage = $jssdk->GetSignPackage();
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,6 +12,7 @@
 	
 	<link rel="stylesheet" type="text/css" href="css/dan.css">
 		<meta name = "viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
+		<script type="text/javascript" src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js "></script>
 		<script data-main = "app.js" type="text/javascript" src = "js/require.js"></script>
 		<script type="text/javascript" src="modules/home/baiduTemplate.js"></script>
 </head>
@@ -33,4 +40,14 @@
 	</li>
 </ul>
 </body>
+<script type="text/javascript">
+  wx.config({
+    debug: true,
+    appId: '<?php echo $signPackage["appId"];?>',
+    timestamp: <?php echo $signPackage["timestamp"];?>,
+    nonceStr: '<?php echo $signPackage["nonceStr"];?>',
+    signature: '<?php echo $signPackage["signature"];?>',
+     jsApiList: []
+  });
+</script>
 </html>
