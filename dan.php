bf18a16d585300529a11d2e5caf3d138464415f1
zhangzhidong <984281302@qq.com>
2016-12-20 11:03:10 +0800

    d

--- dan.php
+++ dan.php
@@ -95,7 +95,6 @@ document.getElementById("b1").onclick = function(){
          longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
         var speed = res.speed; // 速度，以米/每秒计
         var accuracy = res.accuracy; // 位置精度
-        alert(longitude)
     }
 });
 }
